d05dd11af0f5ea45495f567a49da10e885947108
Vasiliy Portko <vpor901@gmail.com>
2025-06-28 21:31:55 +1100

    KTB-02 branch name

---
+++ src/main/kotlin/additional/files.kt
@@ -0,0 +1,14 @@
+package org.example.additional
+
+import java.io.File
+
+fun main() {
+       val wordFile: File = File("word.txt")
+        wordFile.createNewFile()
+
+    val lines = wordFile.readLines()
+
+    for (line in lines) {
+        println(line)
+    }
+}
