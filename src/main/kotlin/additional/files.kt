13d7844607baab212a10fa4355cfe18ea21d6c31
Vasiliy <vpor901@gmail.com>
2025-07-01 21:52:07 +1100

    KTB-04

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -2,13 +2,25 @@ package additional
 
 import java.io.File
 
-fun main() {
-       val wordFile: File = File("word.txt")
-        wordFile.createNewFile()
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
 
+fun main() {
+    val wordFile: File = File("word.txt")
     val lines = wordFile.readLines()
 
+    val dictionary = mutableListOf<Word>()
+
     for (line in lines) {
-        println(line)
+        val line = line.split("|")
+        val correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+        val word = Word(line[0], line[1])
+        dictionary.add(word)
+    }
+    dictionary.forEach { everyWord: Word ->
+        println(everyWord)
     }
 }
