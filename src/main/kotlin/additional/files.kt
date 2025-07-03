3441ccdd5276f034a8d6aa70a0582808a8d96e9c
Vasiliy Portko <vpor901@gmail.com>
2025-07-03 21:07:24 +1100

    KTB-05
    KTB-04

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -1,4 +1,5 @@
 package additional
+
 import java.io.File
 
 data class Word(
@@ -7,21 +8,53 @@ data class Word(
     val correctAnswersCount: Int = 0,
 )
 
-fun main() {
+fun loadDictionary(): List<Word> {
     val wordFile: File = File("word.txt")
-    val lines = wordFile.readLines()
+    val lines = if (wordFile.exists()) {
+        wordFile.readLines()
+    } else {
+        emptyList()
+    }
 
     val dictionary = mutableListOf<Word>()
 
     for (line in lines) {
-        val line = line.split("|")
+       val line =  line.split("|")
         val original = line.getOrNull(0) ?: ""
         val translate = line.getOrNull(1) ?: ""
-        val correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
-        val word = Word(original = original, translate = translate, correctAnswersCount = correctAnswersCount )
-        dictionary.add(word)
+        val correctCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+
+        dictionary.add(Word(original, translate, correctCount))
     }
-    for (word in dictionary) {
-        println(word)
+    return dictionary
+}
+
+
+fun main() {
+    val menuFile = File("menu.txt")
+    menuFile.createNewFile()
+
+    while (true) {
+        menuFile.writeText("1 - Учить слова\n2 - Статистика\n0 - Выход")
+        println(
+            "Программа предназначена для изучения иностранных слов,\n" + "Выберите ваше действие (введите 0 или 1 или 2):"
+        )
+        val menu = menuFile.readLines()
+        for (point in menu) {
+            println(point)
+        }
+
+        val choice = readLine()?.toInt()
+        when (choice) {
+            0 -> {
+                println("Выбран пункт\"выход\"")
+                break
+            }
+
+            1 -> println("Выбран пункт \"учить слова\"")
+            2 -> println("Выбран пункт \"Статистика\"")
+            else -> println("Введите 0 или 1 или 2")
+        }
+        println()
     }
 }
