120083bba441e250c31c9ad6b7377137e6cfd82a
Vasiliy Portko <vpor901@gmail.com>
2025-07-16 09:36:14 +1100

    KTB-09

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,40 @@
+package additional
+
+import java.io.File
+
 data class Word(
     val original: String,
     val translate: String,
-    val correctAnswerCount: Int = 0,
+    var correctAnswerCount: Int = 0,
 )
+
+fun loadDictionary(): MutableList<Word> {
+
+    val fileWord = File("word.txt")
+    val lines = fileWord.readLines()
+
+    val dictionary = mutableListOf<Word>()
+
+    for (line in lines) {
+        val separateCell = line.split("|")
+        val original = separateCell.getOrNull(0) ?: ""
+        val translate = separateCell.getOrNull(1) ?: ""
+        val correctAnswerCount = separateCell.getOrNull(2)?.toIntOrNull() ?: 0
+
+        val word = Word(
+            original = original, translate = translate, correctAnswerCount = correctAnswerCount
+        )
+        dictionary.add(word)
+    }
+    return dictionary
+}
+
+fun saveDictionary(dictionary: List<Word>) {
+
+    val fileWord = File("word.txt")
+    val lines = dictionary.map { "${it.original}|${it.translate}|${it.correctAnswerCount}" }
+    fileWord.writeText(lines.joinToString("\n"))
+}
 
 const val CRITERION_OF_STUDY = 3
 
@@ -10,7 +42,7 @@ const val NUMBER_VARIANTS_IN_ANSWERS = 4
 
 fun main() {
 
-    val trainer = LearnWordsTrainer()
+    val dictionary = loadDictionary()
 
     println("Программа предназначена для изучения иностранных слов\n")
 
@@ -22,28 +54,49 @@ fun main() {
 
         when (choice) {
             1 -> {
+                val notLearnedList = dictionary.filter { it.correctAnswerCount < CRITERION_OF_STUDY }
+
+                if (notLearnedList.isEmpty()) {
+                    println("Вы выучили все слова, поздравляем")
+                    continue
+                }
+
                 while (true) {
+                    val needToAddInVariantsAnswer = NUMBER_VARIANTS_IN_ANSWERS - notLearnedList.size
+                    val learnedList = dictionary.filter { it.correctAnswerCount >= CRITERION_OF_STUDY }
 
-                    val question = trainer.getNextQuestion()
 
-
-                    if (question == null) {
-                        println("Вы выучили все слова, поздравляем")
-                        continue
+                    val questionWord = if (needToAddInVariantsAnswer > 0) {
+                        (notLearnedList + learnedList.shuffled().take(needToAddInVariantsAnswer).shuffled())
+                    } else {
+                        notLearnedList.shuffled().take(NUMBER_VARIANTS_IN_ANSWERS)
                     }
+                    val correctAnswer = questionWord.random()
+                    val options = questionWord.map { it.translate }.shuffled()
+                    val correctAnswerID = options.indexOf(correctAnswer.translate) + 1
 
-                    println("Как переводится: ${question.correctionAnswer.original} ?")
-                    question.variants.forEachIndexed { index, option -> println("${index + 1}. $option") }
+                    println("Как переводится: ${correctAnswer.original} ?")
+                    options.forEachIndexed { index, option -> println("${index + 1}. $option") }
+                    println("\n0 - меню\n")
 
                     println("Введите номер ответа: ")
                     val answer = readLine()?.toInt()
 
-                    if (answer != null && answer in 1..question.variants.size) {
-                        if (question.variants[answer - 1].toString() == question.correctionAnswer.translate) {
-                            println("Правильно.\nОтвет: ${question.variants[answer - 1]}")
+                    if (answer != null && answer in 1..options.size) {
+                        if (answer == correctAnswerID) {
+                            println("Правильно.\nОтвет: ${options[answer - 1]}")
+
+                            val indexInDict = dictionary.indexOfFirst { it.original == correctAnswer.original }
+                            if (indexInDict != -1) {
+                                dictionary[indexInDict].correctAnswerCount++
+                                saveDictionary(dictionary)
+                            }
                         } else {
-                            println("Неверно.\nПравильный ответ: ${question.correctionAnswer.translate}")
+                            println("Неверно.\nПравильный ответ: ${correctAnswer.translate}")
                         }
+                    } else if(answer == 0) {
+                        println("выход в меню")
+                        break
                     } else {
                         println("Некорректный ввод")
                     }
@@ -51,8 +104,117 @@ fun main() {
             }
 
             2 -> {
-                val statistics = trainer.getStatistics()
-                println("результат изучения:\nвыучено ${statistics.learned}/${statistics.total}, ${statistics.percent}%")
+                val totalCount = dictionary.size
+                val learnedCount = dictionary.count { it.correctAnswerCount >= CRITERION_OF_STUDY }
+                val percentCount = if (totalCpackage additional
+
+                    import java.io.File
+
+                    data class Word(
+                    val original: String,
+                    val translate: String,
+                    val correctAnswerCount: Int = 0,
+                )
+
+                fun loadDictionary(): MutableList<Word> {
+
+                    val fileWord = File("word.txt")
+                    val lines = fileWord.readLines()
+
+                    val dictionary = mutableListOf<Word>()
+
+                    for (line in lines) {
+                        val separateCell = line.split("|")
+                        val original = separateCell.getOrNull(0) ?: ""
+                        val translate = separateCell.getOrNull(1) ?: ""
+                        val correctAnswerCount = separateCell.getOrNull(2)?.toIntOrNull() ?: 0
+
+                        val word = Word(
+                            original = original, translate = translate, correctAnswerCount = correctAnswerCount
+                        )
+                        dictionary.add(word)
+                    }
+                    return dictionary
+                }
+
+                const val CRITERION_OF_STUDY = 3
+
+                const val NUMBER_VARIANTS_IN_ANSWERS = 4
+
+                fun main() {
+
+                    val dictionary = loadDictionary()
+
+                    println("Программа предназначена для изучения иностранных слов\n")
+
+                    while (true) {
+                        println(
+                            "выберите ваше действие\n" + "1 - Учить слова\n2 - статистика\n0 - выход"
+                        )
+                        val choice = readLine()?.toInt()
+
+                        when (choice) {
+                            1 -> {
+                                val notLearnedList = dictionary.filter { it.correctAnswerCount < CRITERION_OF_STUDY }
+
+                                if (notLearnedList.isEmpty()) {
+                                    println("Вы выучили все слова, поздравляем")
+                                    continue
+                                }
+
+                                val needToAddInVariantsAnswer = NUMBER_VARIANTS_IN_ANSWERS - notLearnedList.size
+                                val learnedList = dictionary.filter { it.correctAnswerCount >= CRITERION_OF_STUDY }
+                                val questionWord = if (needToAddInVariantsAnswer > 0) {
+                                    (notLearnedList + learnedList.shuffled().take(needToAddInVariantsAnswer).shuffled())
+                                } else {
+                                    notLearnedList.shuffled().take(NUMBER_VARIANTS_IN_ANSWERS)
+                                }
+
+                                val correctAnswer = notLearnedList.random()
+
+                                val options = questionWord.map { it.translate }.shuffled()
+                                println("Как переводится: ${correctAnswer.original} ?")
+                                options.forEachIndexed { index, option -> println("${index + 1}. $option") }
+
+                                println("Введите номер ответа: ")
+                                val answer = readLine()?.toInt()
+
+                                if (answer != null && answer in 1..options.size) {
+                                    if (options[answer - 1] == correctAnswer.translate) {
+                                        println("Правильно.\nОтвет: ${options[answer - 1]}")
+                                    } else {
+                                        println("Неверно.\nПравильный ответ: ${correctAnswer.translate}")
+                                    }
+                                } else {
+                                    println("Некорректный ввод")
+                                }
+                            }
+
+                            2 -> {
+                                val totalCount = dictionary.size
+                                val learnedCount = dictionary.filter { it.correctAnswerCount >= CRITERION_OF_STUDY }.size
+                                val percentCount = if (totalCount != 0) {
+                                    (learnedCount * 100) / totalCount
+                                } else 0
+
+                                println("результат изучения:\nвыучено $learnedCount/$totalCount, $percentCount%")
+                            }
+
+                            0 -> {
+                                println("выбрал выход")
+                                break
+                            }
+
+                            else -> println("некорректный ввод, выберите вариант 0 или 1 или 2")
+
+                        }
+                    }
+                }
+                ount != 0) {
+                    (learnedCount * 100) / totalCount
+                } else 0
+
+                println("результат изучения $percentCount")
             }
 
             0 -> {
@@ -65,4 +227,5 @@ fun main() {
         }
     }
 }
+
 
