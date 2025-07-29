d6aa090c29763822e2a1e44a27a96c8cd6fd77a8
Vasiliy Portko <vpor901@gmail.com>
2025-07-30 00:58:27 +1100

    KTB-13

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -11,14 +11,15 @@ fun main(args: Array<String>) {
     while (true) {
 
         Thread.sleep(2000)
-        val updates: String = getUpdates(botToken)
+        val updates: String = getUpdates(botToken, updateId)
         println(updates)
+        updateId = lastupdateId
     }
 }
 
-fun getUpdates(botToken: Any): String {
+fun getUpdates(botToken: Any, updateId: Int): String {
 
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
