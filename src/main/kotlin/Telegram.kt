26948e176494f3dd9f93eeb3be9b6b9add076f1b
Vasiliy Portko <vpor901@gmail.com>
2025-08-04 21:06:44 +1100

    KTB-14

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -8,22 +8,25 @@ fun main(args: Array<String>) {
     val botToken: String = args[0]
     var updateId = 0
 
+    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val updateIdRegex = "\"update_id\":(\\d+)".toRegex()
+
     while (true) {
 
         Thread.sleep(2000)
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-         updateId = updateIdString.toInt() + 1
+        val updateIds = updateIdRegex.findAll(updates)
+            .map { it.groupValues[1].toInt() }
+            .toList()
 
-        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        if (updateIds.isNotEmpty()) {
+            updateId = updateIds.maxOrNull()!! + 1
+        }
+
         val matchResult: MatchResult? = messageTextRegex.find(updates)
-        val groups =matchResult?.groups
-        val text = groups?.get(1)?.value
+        val text = matchResult?.groups?.get(1)?.value
         println(text)
     }
 }
